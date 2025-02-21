b526785274e429ec3893093378fb946fcd33bc4e
cavelazquez8 <cavelazquez8@gmail.com>
2025-02-21 14:01:07 -0800

    Termux lsp clangd

--- init.lua
+++ init.lua
@@ -656,11 +656,14 @@ require('lazy').setup({
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            -- ertain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
         },
+      }
+      require('lspconfig').clangd.setup {
+        capabilities = capabilities,
       }
     end,
   },
